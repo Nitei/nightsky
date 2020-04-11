de3c4870c08b2af74aa27b9d23a8906bb16104d6
Jorge <jorgecampoy81@gmail.com>
2020-04-11 11:37:31 +0200

    Creada clase para desuscribirse

--- src/app/shared/shared.module.ts/shared.module.ts
+++ src/app/shared/shared.module.ts/shared.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from '../components/button/button.component';
+import { OnlyNumberDirective } from '../directives/only-number/only-number.directive';
 
 
 
 @NgModule({
   declarations: [
-    ButtonComponent
+    ButtonComponent,
+    OnlyNumberDirective
   ],
   exports: [
-    ButtonComponent
+    ButtonComponent,
+    OnlyNumberDirective
   ],
   imports: [
     CommonModule
